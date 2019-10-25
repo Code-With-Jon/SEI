8475ae3cc55541fb5d20875644fcaff544fa948a
Jon <jon@Sciences-MacBook-Pro.local>
2019-10-25 10:45:06 -0700

    added tictactoe template

---
+++ tictactoe/js/main.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+let turn, winner, board
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
